fd1fe68c5c037190d650810d9a7657df0e6e116c
RamS <sreeram596@gmail.com>
2019-12-20 20:48:09 +0530

    flask changes

--- fsk/render1.py
+++ fsk/render1.py
@@ -1,10 +1,17 @@
 from flask import render_template,Flask
 
-app = Flask(__name__, template_folder='/home/rams/Desktop/code/fsk')
+app = Flask(__name__, template_folder='/home/rams/Desktop/code/HTML')
 
+@app.route('/')
+def start_page():
+    return render_template('image.html') 
 @app.route('/hello/<user>')
 def hello_world(user):
-    return render_template('hello.html', fsks=user)
+    return render_template('fsk_hello.html', fsks=user)
+
+@app.route('/index')
+def index():
+    return render_template('fsk_base.html')
 
 
 if __name__ == "__main__":
