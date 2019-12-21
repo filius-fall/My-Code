64549b799c2fdc586695dcc19cb948043824f253
RamS <sreeram596@gmail.com>
2019-12-21 15:34:26 +0530

    login

---
+++ fsk/login.py
@@ -0,0 +1,22 @@
+from flask import Flask,redirect,url_for,request
+
+app = Flask(__name__, template_folder="/home/rams/Desktop/HTML")
+
+
+@app.route('/success/<name>')
+def success(name):
+    return "Hello %s!" %name
+
+
+
+@app.route('/login', methods = ['POST','GET'])
+def login():
+    if request.method=='POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name = user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name = user))
+
+if __name__ == "__main__":
+    app.run(debug=True)
