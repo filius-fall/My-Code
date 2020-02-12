564af2a654aa30739233215025316a4ee1687879
RamS <sreeram596@gmail.com>
2020-02-12 11:16:47 +0530

    12/2/2020

--- sql/commit.py
+++ sql/commit.py
@@ -2,19 +2,41 @@ from sql_tink import db
 db.create_all()
 
 
-from sql_tink import User
-s = int(input('Enter no of users you are adding:'))
-
-for i in range(s):
-    user_name = input('Enter username:')
-    mail = input('Enter email address:')
-    user = User(username = user_name, email = mail)
-    print('%s and %s has been added' %(user_name,mail))
-    db.session.add(user)
-    db.session.commit()
-
-User.query.all()
+from sql_tink import User,Guest
+d = input('Are you User or Guest:')
 
 
+if d == 'User' or d == 'Guest':
+    s = int(input('Enter no of users you are adding:'))
+
+    for i in range(s):
+
+        if d == 'User' :
+            user_name = input('Enter username:')
+            mail = input('Enter email address:')
+            user = User(username = user_name, email = mail)
+            print('%s and %s has been added as user' %(user_name,mail))
+            db.session.add(user)
+            db.session.commit()
+        elif d == 'Guest':
+            name = input('Enter your name:')
+            guest = Guest(name = name)
+            print('%s has been added as guest' %(name))
+            db.session.add(guest)
+            db.session.commit()
+else :
+        print("YOU DON'T BELONG HERE")
 
 
+for c in range(3):
+    b = input('Dp you want to Query all data(y/n):')
+
+    if b == 'y':
+        a = User.query.all()
+
+        print(a)
+    elif b == 'n':
+        print('Thank You')
+    else:
+        print('Invalid Input!! Type either y or n')
+        print('you have %s attempts remaining' %(2-c))
--- sql/sql_tink.py
+++ sql/sql_tink.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/rams/workspace/tinkering/test1.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/rams/workspace/tinkering/test2.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -13,6 +13,13 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+class Guest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Guest %r>' %self.name
 
 if __name__ == "__main__":
     app.run(debug=True)
