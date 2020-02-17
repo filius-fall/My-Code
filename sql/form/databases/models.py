99a81b9ce742c456786a697bef426fcc14a6feb8
RamS <sreeram596@gmail.com>
2020-02-17 11:33:25 +0530

    16/02/2020

--- sql/form/databases/models.py
+++ sql/form/databases/models.py
@@ -1,13 +1,6 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy 
+from databases import db
 from datetime import datetime
 
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,15 +21,3 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-
-
