def4828965a96850736c8293ce6209048e8f73da
RamS <sreeram596@gmail.com>
2020-03-10 12:48:40 +0530

    new

--- ongoing/website/routes.py
+++ ongoing/website/routes.py
@@ -14,7 +14,7 @@ from flask_mail import Message
 @app.route("/home")
 def home():
     page = request.args.get('page',1,type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page,per_page=1)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page,per_page=10)
     return render_template('home.html', posts=posts)
 
 
